98a6d5fa00ecd691e233c95a788ebc9a0f8a17ca
Bikash Patel <bikashpatel506@gmail.com>
2025-04-28 12:26:49 +0530

    Added for docker testing

---
+++ Docker-SpringBoot-WebApp/src/main/java/com/bikash/controller/WishMessage.java
@@ -0,0 +1,19 @@
+package com.bikash.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class WishMessage {
+	
+	@GetMapping("/message/{names}")
+	public ResponseEntity<String> getMessage(@RequestParam String name)
+	{
+		return new ResponseEntity<String>("Good Morning "+name,HttpStatus.OK);
+	}
+}
