d7166d5e39d9dc9c6efca2a56fa41bf5e3854ff2
Bikash Patel <bikashpatel506@gmail.com>
2025-04-29 12:23:17 +0530

    Update .ignore file

--- Docker-SpringBoot-WebApp/src/main/java/com/bikash/controller/WishMessage.java
+++ Docker-SpringBoot-WebApp/src/main/java/com/bikash/controller/WishMessage.java
@@ -3,6 +3,7 @@ package com.bikash.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greetings")
 public class WishMessage {
 	
-	@GetMapping("/message/{names}")
-	public ResponseEntity<String> getMessage(@RequestParam String name)
+	@GetMapping("/message/{name}")
+	public ResponseEntity<String> getMessage(@PathVariable String name)
 	{
 		return new ResponseEntity<String>("Good Morning "+name,HttpStatus.OK);
 	}
