7e6f09280f34a2c0384ad18f2cffa38f06fcd608
Bikash Patel <bikashpatel506@gmail.com>
2025-07-28 14:23:50 +0530

    Updated

--- JRTP-MP02-FrontOfficeTeamPortal/src/main/java/com/bikash/controller/EnquiryManagementController.java
+++ JRTP-MP02-FrontOfficeTeamPortal/src/main/java/com/bikash/controller/EnquiryManagementController.java
@@ -25,78 +25,84 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class EnquiryManagementController {
-    @Autowired
-    private IEnquiryManagementService enquiryService;
-    @Autowired
-    private HttpSession session;
+	@Autowired
+	private IEnquiryManagementService enquiryService;
+	@Autowired
+	private HttpSession session;
 
-    public EnquiryManagementController() {
-    }
+	public EnquiryManagementController() {
+	}
 
-    @GetMapping({"/dashboard"})
-    public String dashboardPage(Map<String, Object> map, @ModelAttribute("dashboard") DashboardResponse response) {
-        DashboardResponse result = this.enquiryService.getDashboard();
-        BeanUtils.copyProperties(result, response);
-        map.put("response", result);
-        return "dashboard";
-    }
+	@GetMapping({ "/dashboard" })
+	public String dashboardPage(Map<String, Object> map, @ModelAttribute("dashboard") DashboardResponse response) {
+		DashboardResponse result = this.enquiryService.getDashboard();
+		BeanUtils.copyProperties(result, response);
+		map.put("response", result);
+		return "dashboard";
+	}
 
-    @GetMapping({"/addenquiry"})
-    public String addEnquiryPage(@ModelAttribute("enquiry") AddEnquiryForm enquiryForm, Map<String, Object> map) {
-        this.loadSelectData(map);
-        return "addenquiry";
-    }
+	@GetMapping({ "/addenquiry" })
+	public String addEnquiryPage(@ModelAttribute("enquiry") AddEnquiryForm enquiryForm, Map<String, Object> map) {
+		this.loadSelectData(map);
+		return "addenquiry";
+	}
 
-    @PostMapping({"/addenquiry"})
-    public String addEnquiryData(@ModelAttribute("enquiry") AddEnquiryForm enquiryForm, RedirectAttributes red) {
-        String successMsg = this.enquiryService.addEnquiry(enquiryForm);
-        red.addFlashAttribute("successMsg", successMsg);
-        return "redirect:/addenquiry";
-    }
+	@PostMapping({ "/addenquiry" })
+	public String addEnquiryData(@ModelAttribute("enquiry") AddEnquiryForm enquiryForm, RedirectAttributes red) {
+		String successMsg = this.enquiryService.addEnquiry(enquiryForm);
+		red.addFlashAttribute("successMsg", successMsg);
+		return "redirect:/addenquiry";
+	}
 
-    @GetMapping({"/editenquirydata"})
-    public String editEnquiryPage(@ModelAttribute("enquiry") EditEnquiryForm enquiryForm, Map<String, Object> map) {
-        this.loadSelectData(map);
-        return "editenquiry";
-    }
+	@GetMapping({ "/editenquirydata" })
+	public String editEnquiryPage(@ModelAttribute("enquiry") EditEnquiryForm enquiryForm, Map<String, Object> map) {
+		this.loadSelectData(map);
+		return "editenquiry";
+	}
 
-    @PostMapping({"/editenquirydata"})
-    public String editEnquiryData(@ModelAttribute("enquiry") EditEnquiryForm enquiryForm, RedirectAttributes red) {
-        String successMsg = this.enquiryService.editEnquiry(enquiryForm);
-        red.addFlashAttribute("successMsg", successMsg);
-        return "redirect:/editenquirydata";
-    }
+	@PostMapping({ "/editenquirydata" })
+	public String editEnquiryData(@ModelAttribute("enquiry") EditEnquiryForm enquiryForm, RedirectAttributes red) {
+		String successMsg = this.enquiryService.editEnquiry(enquiryForm);
+		red.addFlashAttribute("successMsg", successMsg);
+		return "redirect:/viewenquiry";
+	}
 
-    @GetMapping({"/editenquiry"})
-    public String editEnquiryPage(@RequestParam Integer enquiryId, @ModelAttribute("enquiry") EditEnquiryForm enquiryForm, Map<String, Object> map) {
-        EnquiryDetails enquiryDetails = this.enquiryService.getEditDetails(enquiryId);
-        BeanUtils.copyProperties(enquiryDetails, enquiryForm);
-        this.loadSelectData(map);
-        return "editenquiry";
-    }
+	@GetMapping({ "/editenquiry" })
+	public String editEnquiryPage(@RequestParam Integer enquiryId,
+			@ModelAttribute("enquiry") EditEnquiryForm enquiryForm, Map<String, Object> map) {
+		EnquiryDetails enquiryDetails = this.enquiryService.getEditDetails(enquiryId);
+		BeanUtils.copyProperties(enquiryDetails, enquiryForm);
+		this.loadSelectData(map);
+		return "editenquiry";
+	}
 
-    @GetMapping({"/viewenquiry"})
-    public String viewEnquiryPage(Map<String, Object> map) {
-        int id = (Integer)this.session.getAttribute("userId");
-        List<EnquiryDetails> details = this.enquiryService.viewEnquiry(id);
-        this.loadSelectData(map);
-        map.put("enquiryData", details);
-        return "viewenquiry";
-    }
+	@GetMapping({ "/viewenquiry" })
+	public String viewEnquiryPage(Map<String, Object> map, @ModelAttribute("successMsg") String successMsg) {
+		int id = (Integer) this.session.getAttribute("userId");
+		List<EnquiryDetails> details = this.enquiryService.viewEnquiry(id);
+		this.loadSelectData(map);
+		map.put("enquiryData", details);
+		// Add successMsg to map only if it exists
+		if (successMsg != null && !successMsg.isEmpty()) {
+			map.put("successMsg", successMsg);
+		}
 
-    @GetMapping({"/viewenquiry-filter"})
-    public String viewEnquiryPageWithFilter(Map<String, Object> map, @ModelAttribute SearchCriteriaForm searchForm) {
-        int id = (Integer)this.session.getAttribute("userId");
-        List<EnquiryDetails> details = this.enquiryService.viewEnquiryWithFiltered(id, searchForm);
-        this.loadSelectData(map);
-        map.put("enquiryData", details);
-        return "viewenquiryfilter";
-    }
+		return "viewenquiry";
+	}
 
-    public void loadSelectData(Map<String, Object> map) {
-        List<String> courses = this.enquiryService.getCourses();
-        List<String> status = this.enquiryService.getStatus();
-        map.put("courses", courses);
-        map.put("statuses", status);
-    }
+	@GetMapping({ "/viewenquiry-filter" })
+	public String viewEnquiryPageWithFilter(Map<String, Object> map, @ModelAttribute SearchCriteriaForm searchForm) {
+		int id = (Integer) this.session.getAttribute("userId");
+		List<EnquiryDetails> details = this.enquiryService.viewEnquiryWithFiltered(id, searchForm);
+		this.loadSelectData(map);
+		map.put("enquiryData", details);
+		return "viewenquiryfilter";
+	}
+
+	public void loadSelectData(Map<String, Object> map) {
+		List<String> courses = this.enquiryService.getCourses();
+		List<String> status = this.enquiryService.getStatus();
+		map.put("courses", courses);
+		map.put("statuses", status);
+	}
 }
